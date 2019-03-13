ecc9bad9523c3b35dacee2e249c7c6060407220d
adriana-rv <adriana.rv@gmail.com>
2019-03-13 13:32:35 -0600

    Deelte comments

--- src/matem/congresos/content/slidercontent.py
+++ src/matem/congresos/content/slidercontent.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
-# from matem.congresos import _
 from matem.congresos import congresosMessageFactory as _
-# from matem.annualreport.validators import isValidFileType
-# from matem.annualreport.validators import validatesinged
-# from plone import api
-# from plone.autoform import directives
-# from plone.dexterity.content import Item
-# from plone.namedfile.field import NamedBlobFile
 from plone.namedfile import field as namedfile
 from plone.supermodel import model
 from zope import schema
-# from zope.interface import implementer
-# from plone import api
-# from z3c.form.interfaces import IAddForm
-# from zope.i18n import translate
-
 
 class ISliderContent(model.Schema):
     """Dexterity-schema for activities in slider"""
