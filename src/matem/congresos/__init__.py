4ac7e198ee7cbf7bc9be12505900f6e07804b82f
adriana-rv <adriana.rv@gmail.com>
2019-03-13 17:27:48 -0600

    Fix code analysis

--- src/matem/congresos/__init__.py
+++ src/matem/congresos/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Main product initializer
 """
 
@@ -43,8 +44,9 @@ def initialize(context):
     # in the GenericSetup profile.
 
     for atype, constructor in zip(content_types, constructors):
-        utils.ContentInit('%s: %s' % (config.PROJECTNAME, atype.portal_type),
+        utils.ContentInit(
+            '%s: %s' % (config.PROJECTNAME, atype.portal_type),
             content_types=(atype, ),
             permission=config.ADD_PERMISSIONS[atype.portal_type],
             extra_constructors=(constructor,),
-            ).initialize(context)
+        ).initialize(context)
