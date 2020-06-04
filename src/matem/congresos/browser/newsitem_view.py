37c88887d9eee50bd132bd3196e5a1894a10371d
Gilo <gil.cano@gmail.com>
2020-06-03 19:35:23 -0500

    add newsitem browserview

--- src/matem/congresos/browser/newsitem_view.py
+++ src/matem/congresos/browser/newsitem_view.py
@@ -1,22 +1,6 @@
  # -*- coding: utf-8 -*-
-import urllib
-
-from zope.component import getMultiAdapter
-from zope.interface import implements
-from zope.interface import alsoProvides
-from zope.i18n import translate
 from zope.publisher.browser import BrowserView
 
-from AccessControl import Unauthorized
-from Acquisition import aq_parent, aq_inner
-from OFS.interfaces import IOrderedContainer
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from Products.CMFPlone.utils import safe_unicode
-from Products.CMFPlone.utils import pretty_title_or_id, isExpired
-
-from plone.app.content.browser.interfaces import IContentsPage
-from plone.app.content.browser.tableview import Table, TableBrowserView
 
 class NewsItemView(BrowserView):
     """
@@ -26,18 +10,12 @@ class NewsItemView(BrowserView):
         self.context = context
         self.request = request
 
-    @property
-    def portal_catalog(self):
-        return getToolByName(self.context, 'portal_catalog')
-
-    @property
-    def portal(self):
-        return getToolByName(self.context, 'portal_url').getPortalObject()
-
-    def test(self):
+    def image_class(self):
+        """ return wheter image is wider or taller
         """
-        test method
-        """
-        dummy = _(u'a dummy string')
-
-        return {'dummy': dummy}
+        image = self.context.getImage()
+        if image is None:
+            return
+        if image.width < image.height:
+            return 'newsImageContainer img-higher'
+        return 'newsImageContainer img-wider'
