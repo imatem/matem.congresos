2fd170185a4618b026a1444bbb09ef1367b13680
Gilo <gil.cano@gmail.com>
2020-06-06 01:09:06 -0500

    add batchs

--- src/matem/congresos/browser/newsfolder_view.py
+++ src/matem/congresos/browser/newsfolder_view.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from Products.CMFPlone.PloneBatch import Batch
 from zope.publisher.browser import BrowserView
 
 
@@ -11,11 +12,11 @@ class NewsFolderView(BrowserView):
         self.context = context
         self.request = request
 
-    def foldercontents(self):
+    def foldercontents(self, b_size, b_start):
         brains = api.content.find(
             context=self.context,
             portal_type='News Item',
             sort_on='Date',
             sort_order='descending')
-        return brains
-
+        batch = Batch(brains, b_size or 20, int(b_start), orphan=0)
+        return batch
