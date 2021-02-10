42bb293776822e9d5f49abeab3c2e7c897fdaf88
Gilo <gil.cano@gmail.com>
2021-02-10 00:11:51 -0600

    show actual year posters

--- src/matem/congresos/browser/congresosfolder_view.py
+++ src/matem/congresos/browser/congresosfolder_view.py
@@ -2,6 +2,8 @@
 from plone import api
 from zope.publisher.browser import BrowserView
 
+from DateTime import DateTime
+
 
 class CongresosFolderView(BrowserView):
     """
@@ -12,11 +14,28 @@ class CongresosFolderView(BrowserView):
         self.request = request
 
     def foldercontents(self):
+        folders = api.content.find(
+            depth=1,
+            context=self.context,
+            portal_type='Folder')
+
+
+        if folders:
+            brains = api.content.find(
+                context=self.context,
+                start={'query':DateTime('2021/01/01 00:00:00 UTC'), 'range':'min'},
+                sort_on='start',
+                sort_order='descending')
+            return brains
+
         brains = api.content.find(
             context=self.context,
             sort_on='start',
             sort_order='descending')
         return brains
+
+
+
 
     def eventplace(self, congress):
         try:
